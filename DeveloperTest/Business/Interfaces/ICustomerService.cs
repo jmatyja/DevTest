ebcdfe349b5d20970740626d38d3adf44a2d9e77
Janusz Matyja <janusz.matyja@capgemini.com>
2020-08-25 09:35:19 +0200

    add I customer service

---
+++ DeveloperTest/Business/Interfaces/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using DeveloperTest.Models;
+
+namespace DeveloperTest.Business.Interfaces
+{
+    public interface ICustomerService
+    {
+        CustomerModel[] Getcustomers();
+
+        CustomerModel GetCustomer(int customerId);
+
+        CustomerModel CreateCustomer(BaseCustomerModel model);
+    }
+}
