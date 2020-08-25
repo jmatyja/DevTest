cb133e7f08ec491449781e0c46e9df7fd541ebbb
Janusz Matyja <janusz.matyja@capgemini.com>
2020-08-25 10:22:05 +0200

    update database

--- DeveloperTest/Migrations/20200825082013_Add_Customer.Designer.cs
+++ DeveloperTest/Migrations/20200825082013_Add_Customer.Designer.cs
@@ -4,14 +4,16 @@ using DeveloperTest.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DeveloperTest.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200825082013_Add_Customer")]
+    partial class Add_Customer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
