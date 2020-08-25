230fb10163800145144ee12e0dce40b36ffd7ce1
Janusz Matyja <janusz.matyja@capgemini.com>
2020-08-25 09:41:32 +0200

    Create models

---
+++ DeveloperTest/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeveloperTest.Models
+{
+    public class Customer: CustomerBase
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ DeveloperTest/Models/CustomerBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DeveloperTest.Models
+{
+    public abstract class CustomerBase
+    {
+        public string Name { get; set; }
+
+        public CustomerType Type { get; set; }
+    }
+}
