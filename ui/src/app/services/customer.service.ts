7e81735b5ecdc2abe6a0d10ff5a862cea4b43b2c
Janusz Matyja <janusz.matyja@capgemini.com>
2020-08-25 10:49:54 +0200

    add customer service

---
+++ ui/src/app/services/customer.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Customer } from '../models/customer.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public GetCustomers(): Observable<Customer[]> {
+    return this.httpClient.get<Customer[]>('http://localhost:63235/customer');
+  }
+
+  public GetCustomer(id: number): Observable<Customer> {
+    return this.httpClient.get<Customer>(`http://localhost:63235/customer/${id}`);
+  }
+}
